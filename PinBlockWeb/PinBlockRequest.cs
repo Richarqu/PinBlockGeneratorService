ebc3791d73aea4d66b483a06a837961608196103
Richarqu <richarque@gmail.com>
2020-05-31 00:44:00 +0100

    created pin block request and response

---
+++ PinBlockWeb/PinBlockRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PinBlockWeb
+{
+    public class PinBlockRequest
+    {
+        public string PAN { get; set; }
+        public string PIN { get; set; }
+        public string ZPK { get; set; }
+        public string COMP1 { get; set; }
+        public string COMP2 { get; set; }
+    }
+}
---
+++ PinBlockWeb/PinBlockRes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PinBlockWeb
+{
+    public class PinBlockRes
+    {
+        public string ResponseCode { get; set; }
+        public string ResponseDescription { get; set; }
+        public string PinBlock { get; set; }
+    }
+}
